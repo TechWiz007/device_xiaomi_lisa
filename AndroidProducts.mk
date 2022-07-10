8692eb32f9b52a4e7921612ab2b90ad8cf7e9486
TechWiz007 <gajarebhargav@gmail.com>
2022-07-11 01:44:51 +0530

    lisa: ArrowOS initial changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 PixelOS
+# Copyright (C) 2022 ArrowOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lisa.mk
+    $(LOCAL_DIR)/arrow_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lisa-user \
-    aosp_lisa-userdebug \
-    aosp_lisa-eng
+    arrow_lisa-user \
+    arrow_lisa-userdebug \
+    arrow_lisa-eng
--- arrow_lisa.mk
+++ arrow_lisa.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 PixelOS
+# Copyright (C) 2022 ArrowOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,16 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common PixelOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+ARROW_GAPPS := true
+IS_PHONE := true
 TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+DEVICE_MAINTAINER := Bhargav Gajare
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_lisa
+PRODUCT_NAME := arrow_lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
