16b44702aceb656960155810f8cd8dbf8002053a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-11 01:54:30 +0530

    lisa: include firmware in OTA payload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,9 @@ SOONG_CONFIG_dolby_vision_enabled := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# Firmware
+-include vendor/xiaomi/lisa-firmware/BoardConfigVendor.mk
+
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
